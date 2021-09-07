101a0049a84d9102f2856a3c77e3aa95bdd62166
keito yoshida <ksw2070261@stu.o-hara.ac.jp>
2021-09-07 12:30:04 +0900

    お問い合わせフォーム

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,11 @@
+from typing import Collection
 from django.views import generic
+
+from .forms import InquiryFrom
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    from_class = InquiryFrom
