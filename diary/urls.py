e7f35f30990dc9b519549d7cad8774b37b92fa63
keito yoshida <ksw2070261@stu.o-hara.ac.jp>
2021-09-13 12:45:17 +0900

    認証機能完成

--- diary/urls.py
+++ diary/urls.py
@@ -1,10 +1,20 @@
+from config.settings import MEDIA_ROOT, MEDIA_URL
 from django.urls import path
 from django.urls.conf import include
 from .import views
+
+# from django.contrib import admin
+# from django.contrib.staticfiles.urls import static
+# from django.urls import path,include
+
+# from .import settings_common,settings_dev
 
 app_name = 'diary' 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
-    path('accounts/',include('allauth.urls')),
+    # path('accounts/',include('allauth.urls')),
 ]
+
+# urlpatterns += static(settings_common.MEDIA_URL,
+# document_root=settings_dev.MEDIA_ROOT)
