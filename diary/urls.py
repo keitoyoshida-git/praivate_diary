043e03ba33f00dc96462bd7bfa5787aafd9a5bb3
keito yoshida <ksw2070261@stu.o-hara.ac.jp>
2021-09-14 12:32:22 +0900

    データベースとの連携

--- diary/urls.py
+++ diary/urls.py
@@ -9,10 +9,15 @@ from .import views
 
 # from .import settings_common,settings_dev
 
+from django.urls import path
+
+from .import views
+
 app_name = 'diary' 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
+    path('diary-list',views.DiaryListView.as_view(), name="diary_list")
     # path('accounts/',include('allauth.urls')),
 ]
 
